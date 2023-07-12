f1f18695f745342fd4f134435a4e43a0f7b5e564
elissonsayago <128489709+elissonsayago@users.noreply.github.com>
2023-07-12 12:23:58 -0300

    Add files via upload

--- handler.py
+++ handler.py
@@ -1,11 +1,10 @@
-import os
 import pandas as pd
 import pickle
 from flask import Flask, request
-from cotador_resid_pred import CotadorResidPred
+import os
 
 #load model
-model=pickle.load(open('model_cotador.pkl','rb'))
+model=pickle.load(open('svm_model_cotador.pkl','rb'))
 
 #instantiate Flask
 app = Flask ( __name__)
@@ -22,19 +21,25 @@ def predict():
             df_raw = pd.DataFrame (test_json, columns=test_json[0].keys())
     
 
-    #instantiate data preparation
-    pipeline = CotadorResidPred()
+    #convertendo as colunas para o tipo original correto
+    df_raw['COD_CORRETOR'] = df_raw['COD_CORRETOR'].astype(float)
+    df_raw['FLG_OPERACAO_ESPECIAL'] = df_raw['FLG_OPERACAO_ESPECIAL'].astype(object)
+    df_raw['COD_TIPO_SEGURO'] = df_raw['COD_TIPO_SEGURO'].astype(float)
+    df_raw['COD_GRUPO_ATIVIDADE'] = df_raw['COD_GRUPO_ATIVIDADE'].astype(float)
+    df_raw['COD_TIPO_CONSTRUCAO'] = df_raw['COD_TIPO_CONSTRUCAO'].astype(float)
+    df_raw['FLG_POSSUI_CRITICA'] = df_raw['FLG_POSSUI_CRITICA'].astype(object)
+    df_raw['FLG_EXIGE_VISTORIA'] = df_raw['FLG_EXIGE_VISTORIA'].astype(object)
+    df_raw['VAL_PREMIO_AVISTA'] = df_raw['VAL_PREMIO_AVISTA'].astype(float)
+    df_raw['COD_FORMA_PAGTO'] = df_raw['COD_FORMA_PAGTO'].astype(float)
 
-    #data preparation
-    df1 = pipeline.datapreparation(df_raw)
 
     #prediction
-    pred = model.predict ( df1 )
-    df1['prediction'] = pred
-    return df1.to_json(orient='records')
+    pred = model.predict ( df_raw )
+    df_raw['PREDICTION_FLG_PROPOSTA'] = pred
+    return df_raw.to_json(orient='records')
 
 if __name__ == '__main__':
-    #start flask depois de instalar o scikit-learn
-    port=os.environ.get('PORT',5000)
+    #start flask
     #app.run( host='0.0.0.0', port='5000')
+    port=os.environ.get('PORT',5000)
     app.run( host='0.0.0.0', port=port)
